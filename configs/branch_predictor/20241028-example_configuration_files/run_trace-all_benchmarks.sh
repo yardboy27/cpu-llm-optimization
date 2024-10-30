275f193b735d7eb0761e387a8a8158c4bd81b73b
Bailey Faulk <baileyfaulk2003@outlook.com>
2024-10-30 10:55:17 -0500

    edited .sh files and champsim build to create a new bin directory to hold the most recent champsim build, allows for multiple champsim builds to be run at one time

--- configs/branch_predictor/20241028-example_configuration_files/run_trace-all_benchmarks.sh
+++ configs/branch_predictor/20241028-example_configuration_files/run_trace-all_benchmarks.sh
@@ -2,8 +2,18 @@ set -a
 source ./../../../.env
 set +a
 
-export BUILD_JOB_ID=$(sbatch --parsable --mail-type=$MAIL_TYPE --mail-user=$MAIL_USER build_champsim.slurm)
-export BUILD_JOB_DATE=$(date +"%Y-%m-%d_%H:%M:%S")
+export BUILD_JOB_DATE=$(date +"%Y%m%d%H%M%S")
 
+# Store the current directory
+SLURM_DIRECTORY=$(basename $(pwd))
+export CHAMPSIM_BIN_DIR="$CHAMPSIM/bin/$SLURM_DIRECTORY-d$(date +"%Y%m%d%H%M%S")"
+mkdir -p $CHAMPSIM_BIN_DIR
+
+export BUILD_JOB_ID=$(sbatch --parsable --mail-type=$MAIL_TYPE --mail-user=$MAIL_USER build_champsim.slurm)
+
+export CHAMPSIM_BIN="$CHAMPSIM_BIN_DIR/champsim"
 export TRACE_DIR=$TRACES/all_benchmarks
+
+sleep 15
+
 sbatch --dependency=afterok:$BUILD_JOB_ID --array=0-9 --mail-type=$MAIL_TYPE --mail-user=$MAIL_USER trace_array.slurm
